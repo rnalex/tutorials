7430c2c1f7773a7e2a57f62db163f71e2ca69ef1
RN Alex <rnalex@yahoo.com>
2015-08-14 02:08:20 -0700

    Cyclic sorted array binary search - revised

--- cyclic_binsearch.c
+++ cyclic_binsearch.c
@@ -17,7 +17,7 @@
 
 #define MID(l,r) (l+((r-l)/2))
 
-int test_array[10]={8,9,10,11,12,13,14,15,16,7};
+int test_array[10]={18,9,10,11,12,13,14,15,16,17};
 
 /*
  * Either the array is sorted or
@@ -32,13 +32,14 @@ greatest(int *array, int l , int r)
 	int g1,g2;
 
 	assert(l<=r);
+	printf("\n %d - %d",l,r);
 	/* Sorted arrays has greatest elements at the right*/
 	if (array[l] <= array[r]) return array[r];
 
 	/*If the midpoint is > l the left half is sorted - compare the right half*/
 	if (array[MID(l,r)] > array[l]) {
 		g1 = array[MID(l,r)];
-		g2 = greatest(array,MID(l,r),r);
+		g2 = greatest(array,MID(l,r)+1,r);
 		return (g1 > g2 ? g1 : g2);
 	}
 	/* If the midpoint is < l the left half is unsorted - compare the right half*/
@@ -52,6 +53,8 @@ greatest(int *array, int l , int r)
 int
 main(int argc, char **argv)
 {
-	printf("Greatest %d ", greatest(test_array,0,9));	
+	printf("\nGreatest %d ", greatest(test_array,0,1));	
+	printf("\nGreatest %d ", greatest(test_array,1,9));	
+	printf("\nGreatest %d ", greatest(test_array,0,9));	
 	return 0;
 }
