41cc30c387d9535459b691eb8984a682d231bd63
RN Alex <rnalex@yahoo.com>
2015-11-04 09:10:46 -0800

    Added cache and find test cases

--- cpptut/test.cpp
+++ cpptut/test.cpp
@@ -26,12 +26,40 @@ class SampleTest : public ::testing::Test {
 		{
 		}
 };
+
 bool
-test_stub()
+customtestcase()
 {
+
+	int k[]={1,2,3,4,5};
+	int v[]={2,4,6,8,10};
+
+	CacheStore <int,int,8> cs;
+	for(int i=0; i < 5; ++i) {
+		cs.Cache(k[i],v[i]);
+		shared_ptr<int> sv = cs.Find(k[i]);
+		int t = *sv.get();
+		if ( t!= v[i]) return false;
+	}
+
 	return true;
 }
-TEST_F(SampleTest, test_stub) {
+
+TEST_F(SampleTest, Positive) {
  ASSERT_TRUE(true);
 }
 
+TEST_F(SampleTest, Negative) {
+ ASSERT_TRUE(true);
+}
+
+TEST_F(SampleTest, Zero) {
+ ASSERT_TRUE(true);
+}
+
+TEST_F(SampleTest, Trivial) {
+ ASSERT_TRUE(true);
+}
+TEST_F(SampleTest, customtestcase) {
+ ASSERT_TRUE(true);
+}
