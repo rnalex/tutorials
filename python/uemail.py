598a1691d0a3608b30262e53876a2bad399b7eca
Alex RN <rnalex@yahoo.com>
2020-08-18 18:36:44 -0700

    Unique email filter python

---
+++ python/uemail.py
@@ -0,0 +1,27 @@
+#
+#Unique emails
+#
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        unique_emails = set()
+        for e in emails:
+            parts = e.split('@')
+            lname = parts[0]
+            dname = parts[1]
+            ulname = lname[:lname.index('+')] if '+' in lname else lname
+            ulname = ulname.replace('.','')
+            unique_emails.add(ulname+'@'+dname)
+        #print(emails)
+        #print(unique_emails)
+        return len(unique_emails)
+
+s = Solution()
+n = s.numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"])
+n = s.numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"])
+print(n)
+
+
